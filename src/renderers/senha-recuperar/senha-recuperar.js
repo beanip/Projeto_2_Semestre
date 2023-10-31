b2a6edd599d6d22900e6ff274d78cc313c8a57c6
Pedro <pedro.scarpellini@gmail.com>
2023-10-30 22:34:49 -0300

    Criado o layout e CSS da tela recuperar senha

--- src/renderers/senha-recuperar/senha-recuperar.js
+++ src/renderers/senha-recuperar/senha-recuperar.js
@@ -1,9 +1,14 @@
 const ipcRenderer = require('electron').ipcRenderer
 
 let btnValidar = document.getElementById('btn-validar'),
+    btnVoltar = document.getElementById('btn-voltar'),
     txtEmail = document.getElementById('inp-email')
 
 btnValidar.addEventListener('click', e => {
     ipcRenderer.send('enviarCodigoRecuperacaoEmail', txtEmail.value)
     ipcRenderer.send('navegar', 'senha-codigo')
+})
+
+btnVoltar.addEventListener('click', e => {
+    ipcRenderer.send('navegar', 'login')
 })
